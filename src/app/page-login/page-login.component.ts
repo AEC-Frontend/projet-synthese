8b0f96cfb7d247f04131be1e2efee9d2afe06c5c
SanaG1002 <92936779+SanaG1002@users.noreply.github.com>
2023-03-08 22:43:19 -0500

    PSA-3: Création de la page Login

---
+++ src/app/page-login/page-login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-page-login',
+  templateUrl: './page-login.component.html',
+  styleUrls: ['./page-login.component.scss']
+})
+export class PageLoginComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+ 
+}
