790238067afab1ffc084054f93384720587a0419
jnadeau22 <jnadeau@mcjconseil.com>
2023-03-20 20:44:56 -0400

    fix sidenav behavior

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent {
+  constructor(private router: Router) {}
 
+  isLoginPage() {
+    return this.router.url === '/page-login';
+  }
+
+  ngDoCheck() {
+    console.log(this.isLoginPage(), this.router.url);
+  }
 }
