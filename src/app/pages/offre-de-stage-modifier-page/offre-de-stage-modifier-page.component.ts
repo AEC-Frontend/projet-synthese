40d58cf97883bb99b44c81a4a3a52dbaaa744d8e
jnadeau22 <jnadeau@mcjconseil.com>
2023-04-07 16:02:25 -0400

    fix rebase conflict

---
+++ src/app/pages/offre-de-stage-modifier-page/offre-de-stage-modifier-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offre-de-stage-modifier-page',
+  templateUrl: './offre-de-stage-modifier-page.component.html',
+  styleUrls: ['./offre-de-stage-modifier-page.component.scss']
+})
+export class OffreDeStageModifierPageComponent {
+
+}
